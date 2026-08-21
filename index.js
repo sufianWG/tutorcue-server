145860dfbca8b76c59e7b299bd07e88699d0aa8e
sufianWG <webgurumrmbd@gmail.com>
2026-08-21 08:03:41 +0600

    connected to mongodb & created tutors api

--- index.js
+++ index.js
@@ -4,15 +4,38 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const express = require('express');
 const dotenv = require("dotenv");
 dotenv.config();
+const { MongoClient, ObjectId } = require("mongodb");
 const app = express()
 const port = process.env.PORT || 6028;
 
 app.use(express.json());
 
+
+const client = new MongoClient(process.env.MONGODB_URI);
+async function connectToMongoDB() {
+    try {
+        await client.connect();
+        
+        app.get("/tutors", async (req, res) => {
+            const db = client.db("tutorcue");
+            const tutorsCollection = db.collection("tutors");
+            const tutors = await tutorsCollection.find().toArray();
+            // console.log(tutors);
+            res.json(tutors);
+        });
+        // console.log("You successfully connected to MongoDB!");
+        return client;
+    } catch (err) {
+        console.dir(err);
+    }
+}
+
+connectToMongoDB()
+
 app.get('/', (req, res) => {
-  res.send('Hello World from express server!')
+    res.send('Hello World from express server!')
 })
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
+    console.log(`Server running at http://localhost:${port}`)
 })
