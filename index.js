1520e00c4eeb51b2faec8a6f07d2c415d624a5a1
sufianWG <webgurumrmbd@gmail.com>
2026-08-26 21:01:45 +0600

    again tried to fix issue

--- index.js
+++ index.js
@@ -47,6 +47,9 @@ app.get('/', (req, res) => {
     res.send('Hello World from express server!')
 })
 
+//  have to remove comment out
+module.exports = app;
+
 // app.listen(port, () => {
 //     console.log(`Server running at http://localhost:${port}`)
 // })
